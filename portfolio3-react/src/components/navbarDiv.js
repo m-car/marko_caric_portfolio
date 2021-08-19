bc71079a71e4317187c287e66e6ff5d8d5e4c21d
Marko Caric <pbmc117@gmail.com>
2021-08-18 20:21:58 -0700

    added attributes to buttons for navbar

--- portfolio3-react/src/components/navbarDiv.js
+++ portfolio3-react/src/components/navbarDiv.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-function NavbarDiv() {
+function NavbarDiv(props) {
   return (
 <div>
    <h1>Marko Caric</h1>
    <nav>
-   <button>about me</button>
-   <button>work</button>
-   <button>contact me</button>
+   <button type="button"
+        className="btn btn-primary"
+        onClick={props.handleIncrement}>about me</button>
+   <button type="button"
+        className="btn btn-primary"
+        onClick={props.handleIncrement}>work</button>
+   <button type="button"
+        className="btn btn-primary"
+        onClick={props.handleIncrement}>contact me</button>
    </nav>
 
 </div>
--- portfolio3-react/src/displayMain.js
+++ portfolio3-react/src/displayMain.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import HelloDiv from './components/helloDiv';
 import NavbarDiv from './components/navbarDiv';
 import MyworkDiv from './components/myworkDiv';
 import ContactmeDiv from './components/contactmeDiv';
-
 export default function DisplayMain(){
+    const  [view, setView] = useState(HelloDiv);
+
+    const handleView=() =>{
+
+    }
+
     return(
         <div>
         <NavbarDiv />
