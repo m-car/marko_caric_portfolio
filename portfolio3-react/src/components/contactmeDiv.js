fcd67be7925a4a8705811e37520e5bd0acda8d02
Marko Caric <pbmc117@gmail.com>
2021-08-18 18:23:21 -0700

    Added my contact information into variables for contactmeDiv component

--- portfolio3-react/src/components/contactmeDiv.js
+++ portfolio3-react/src/components/contactmeDiv.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
+const myEmail = 'pbmc117@gmail.com';
+const myGithub='https://github.com/m-car';
+const myLinkedIn='https://www.linkedin.com/in/markocaric/'
 
 function ContactmeDiv() {
     return(
         <div>
             <ul>
-                <li>email </li>
-                <li>github </li>
-                <li>linkedin </li>
+                
+                <li><a href={myEmail}>Email Me</a></li>
+                <li><a href={myGithub}>My GitHub</a> </li>
+                <li><a href={myLinkedIn}>LinkedIn</a></li>
                 <li>back to top </li>
             </ul>
         </div>
