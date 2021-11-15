14a1f6faa7255dac77976a2ff57bded0a7ab3708
Marko Caric <pbmc117@gmail.com>
2021-11-15 13:53:34 -0800

    Added email to footerDiv.js

--- portfolio3-react/src/components/FooterDiv.js
+++ portfolio3-react/src/components/FooterDiv.js
@@ -2,23 +2,29 @@ import React from 'react';
 import './style.css'
 
 export default function FooterDiv() {
-    return (
-        <div className="footer">
-        <div class="social_links">
-          <a href="https://github.com/m-car">
-            <span className="fa-stack fa-lg gt combo">
-              <i className="fa fa-circle fa-stack-2x circle">github</i>
-              <i className="fa fa-github fa-stack-1x fa-inverse icon"></i>
-            </span>
-          </a>
-          <a href="https://www.linkedin.com/in/markocaric/">
-            <span className="fa-stack fa-lg tw combo">
-              <i className="fa fa-circle fa-stack-2x circle">linkedin</i>
-              <i className="fa fa-linkedin fa-stack-1x fa-inverse icon"></i>
-            </span>
-          </a>
-        </div>
+  return (
+    <div className="footer">
+      <div class="social_links">
+        <a href="https://github.com/m-car">
+          <span className="fa-stack fa-lg gt combo">
+            <i className="fa fa-circle fa-stack-2x circle">github</i>
+            {/* <i className="fa fa-github fa-stack-1x fa-inverse icon"></i> */}
+          </span>
+        </a>
+        <a href="https://www.linkedin.com/in/markocaric/">
+          <span className="fa-stack fa-lg tw combo">
+            <i className="fa fa-circle fa-stack-2x circle">linkedin</i>
+            <i className="fa fa-linkedin fa-stack-1x fa-inverse icon"></i>
+          </span>
+        </a>
+        <a href="mailto: pbmc117@gmail.com">
+          <span className="fa-stack fa-lg tw combo">
+            <i className="fa fa-circle fa-stack-2x circle">email</i>
+            <i className="fa fa-linkedin fa-stack-1x fa-inverse icon"></i>
+          </span>
+        </a>
       </div>
-    );
-  }
-    
+    </div>
+  );
+}
+
